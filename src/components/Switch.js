34d80af36581e0165cec429e4a33e5d8a5b6d05f
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2024-03-15 09:06:31 +0000

    122eded

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -5,7 +5,7 @@ const Switch= ()=>{
         <ul>
             <li><Link to={""} >Home</Link></li>
             <li><Link to={"/about"} >About</Link></li>
-            <Outlet />
+           
         </ul>
     )
 }
